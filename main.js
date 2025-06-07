1d6b6bc942d53125faafc97bf4e2883f06431af3
Ahmed Taha Ozpinar <a.tahaozpinar@gmail.com>
2025-06-08 01:08:48 +0300

    Sembol eklendi, menu bar kaldırıldı, sağ tık eklendi

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain, Menu } = require("electron");
 const { exec } = require("child_process");
 const path = require("path");
 
@@ -6,13 +6,25 @@ app.whenReady().then(() => {
   const myWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: __dirname + "/assets/yt_icon.png",
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
   });
 
+  Menu.setApplicationMenu(null);
+
   myWindow.loadFile("index.html");
+
+  myWindow.webContents.on("context-menu", (event, params) => {
+    const menu = Menu.buildFromTemplate([
+      { label: "Cut", role: "cut" },
+      { label: "Copy", role: "copy" },
+      { label: "Paste", role: "paste" },
+    ]);
+    menu.popup(myWindow);
+  });
 
   ipcMain.on("start-download", (event, { url, format }) => {
     if (!url || !format) {
